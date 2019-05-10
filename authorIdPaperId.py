39be2f889831d78fc1cdbaf2494642f0296e8e3e
Azqual <17307971337@163.com>
2019-05-11 02:20:01 +0800

    init

---
+++ authorIdPaperId.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+import importlib,sys
+importlib.reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+# 将 (AuthorId, PaperId) 设计成类，表示我要 训练／预测 的样本
+class AuthorIdPaperId(object):
+    def __init__(self, authorId, paperId):
+        self.authorId = authorId
+        self.paperId = paperId
+        self.label = None  # 类标
